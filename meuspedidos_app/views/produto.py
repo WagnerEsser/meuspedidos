90d67b38ba5438a5832e9fc1f5791ee9c7520ec7
Wagner Esser <waesser@gmail.com>
2019-01-17 15:08:12 -0200

    Adionado funcionalidade número múltiplo

--- meuspedidos_app/views/produto.py
+++ meuspedidos_app/views/produto.py
@@ -8,9 +8,11 @@ from meuspedidos_app.views.pedido import PedidoView
 class ProdutoView(View):
 
     @classmethod
-    def Listar(self, request):
+    def Listar(self, request, msg=None, tipo_msg=None):
         context_dict = {}
         context_dict['produtos'] = ProdutoModel.objects.all().order_by('nome')
+        context_dict['msg'] = msg
+        context_dict['tipo_msg'] = tipo_msg
         return render(request, 'produtos.html', context_dict)
 
     @classmethod
@@ -24,6 +26,7 @@ class ProdutoView(View):
         if request.session and 'pedido' in request.session:
             id_pedido = request.session['pedido']
             id_produto = request.POST.get('id_produto')
+            produto = ProdutoModel.objects.get(pk=id_produto)
             preco_pago = request.POST.get('preco_pago')
             qtd = request.POST.get('qtd')
 
@@ -33,6 +36,10 @@ class ProdutoView(View):
                 from decimal import Decimal
                 preco_pago = Decimal(preco_pago.replace(',', ''))
                 qtd = int(qtd)
-                PedidoView.adicionar_item(id_pedido, id_produto, preco_pago, qtd)
+
+                if qtd % produto.multiplo == 0:
+                    PedidoView.adicionar_item(id_pedido, id_produto, preco_pago, qtd)
+                else:
+                    return self.Listar(request, msg="A quantidade tem que obedecer o número Múltiplo!", tipo_msg="red")
 
         return self.Listar(request)
